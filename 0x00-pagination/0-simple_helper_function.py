64dbcc9af8ef1e52407e7e0a1f5110e5af930f99
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-12-25 10:30:11 +0300

    this function returns a tuple containing a start index and an end index for a list of particular pagination parameters

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Script that Calculates start and & indx for pagination
+"""
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """
+    Calculate start & end indx for pagination
+
+    Args:
+        page: current page number
+        page_size: number of items per page
+
+    Returns:
+        tuple: tuple containing start and end indices
+    """
+    # page num to 0-indexed (page - 1)
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return start_index, end_index
