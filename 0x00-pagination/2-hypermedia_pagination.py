6748a5189b42fc6fa758fc29a7fc02c631903ffe
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-12-25 12:18:54 +0300

    Gets hypermedia info for db based using provided page num & page size

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -83,7 +83,7 @@ class Server:
         total_pages = math.ceil(len(self.dataset()) / page_size)
         current_page = page
         next_page = current_page + 1 if end_index < total_pages else None
-        prev_page = current_page - 1 if start_index > 0 else None
+        prev_page = current_page - 1 if current_page > 1 else None
 
         return {
             'page_size': len(self.get_page(page, page_size)),
