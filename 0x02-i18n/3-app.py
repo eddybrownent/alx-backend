96658b43fd54840fddda6a3b6c25066ad3f8ef8a
edwin karanja mburu <edwin@eddy-kali.browns.com>
2024-01-09 19:57:42 +0300

    making the files executable

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -33,7 +33,7 @@ def get_locale() -> str:
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-babel.init_app(app, locale_selector=get_locale)
+# babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/')
