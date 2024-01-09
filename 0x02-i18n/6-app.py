b524b8eed46d09f5ac8aba949c9689ec16203578
edwin karanja mburu <edwin@eddy-kali.browns.com>
2024-01-09 22:20:06 +0300

    Changed get_locale function to use a user’s preferred local if it is supported

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+"""
+Flask app
+"""
+from typing import Union, Dict
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """
+    class to config Flask app
+    has a LANGUAGES
+    Babel’s default locale and timezone
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    determineS best match with supported languages
+    """
+    requested_locale = request.args.get('locale')
+
+    if requested_locale and requested_locale in app.config['LANGUAGES']:
+        return requested_locale
+
+    if g.user and g.user['locale'] in app.config["LANGUAGES"]:
+        return g.user['locale']
+
+    request_locale = request.headers.get('Accept-Language')
+
+    if request_locale in app.config["LANGUAGES"]:
+        return request_locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+# babel.init_app(app, locale_selector=get_locale)
+
+
+def get_user(user_id: int) -> Union[Dict, None]:
+    """
+    Get user information based on user ID
+
+    Args:
+        user_id (int): The ID of the user to retrieve
+
+    Returns:
+        Union[Dict, None]]]: dict containing user infor
+        or None if the user ID is not found
+    """
+    return users.get(user_id)
+
+
+@app.before_request
+def before_request():
+    """
+    to find a user if any
+    """
+    user_id = request.args.get('login_as')
+    g.user = get_user(int(user_id)) if user_id else None
+
+
+@app.route('/')
+def hello_Hoblerton() -> str:
+    """
+    simply outputs “Hello world”
+    """
+    return render_template('6-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
